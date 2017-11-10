ffc210af1987e5fbb70251c58d24def9a5085e39
Masood Delfarah <m.delfarah@gmail.com>
2017-11-10 09:46:16 -0500

    Let's go

--- test.cpp
+++ test.cpp
@@ -1,37 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <map>
 #include <algorithm>
 #include <string>
 #include <queue>
+#include <set>
 using namespace std;
 
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-int findContentChildren(vector<int>& g, vector<int>& s) { //s are cookies
-	sort(g.begin(), g.end());
-	sort(s.begin(), s.end());
-	
-	int ret_val = 0;
-	for(int i=0; (i<g.size() && i<s.size() );i++)
-		if( g[i] <= s[i] )
-			ret_val++;
-
-	return ret_val;	
-}
+    bool containsDuplicate(vector<int>& nums) {
+        set<int> mySet;
+        for(auto n: nums){
+			if(mySet.find(n) == mySet.end())
+				mySet.insert(n);
+			else
+				return true;
+		}
+		
+		return false;
+    }
 
 
 int main(){
-
-	vector<int> vect1 = {10,9,8,7};
-	vector<int> vect2 = {5,6,7,8};
-	cout<<findContentChildren(vect1,vect2)<<endl;
+	vector<int> a({1,4,2,6,6});
+	cout<<containsDuplicate(a)<<endl;
 	return 0;
-
 }
